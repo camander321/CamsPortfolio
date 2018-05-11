42b5ac3f9c1086c91302d9609d2dec0943d45f9c
camander321 <chamburg321@gmail.com>
2018-05-11 15:30:46 -0700

    add background image and fix text to be visible

--- Portfolio/wwwroot/js/Home.js
+++ Portfolio/wwwroot/js/Home.js
@@ -8,10 +8,18 @@
     $(".info-btn-about").click(function () {
         $(".info-contact").hide();
         $(".info-about").fadeToggle();
+
+        $('html, body').animate({
+            scrollTop: $(".info-btn-about").offset().top
+        }, 2000);
     });
 
     $(".info-btn-contact").click(function () {
         $(".info-about").hide();
         $(".info-contact").fadeToggle();
+
+        $('html, body').animate({
+            scrollTop: $(".info-btn-contact").offset().top
+        }, 2000);
     });
 });
