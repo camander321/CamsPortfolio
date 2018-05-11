415b2d559d32cad965e4ba1185e040919fe23883
camander321 <chamburg321@gmail.com>
2018-05-11 14:01:38 -0700

    add contact and about sections

--- Portfolio/Models/GithubRepo.cs
+++ Portfolio/Models/GithubRepo.cs
@@ -12,7 +12,7 @@ namespace Portfolio.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string URL { get; set; }
+        public string Html_Url { get; set; }
 
         public static List<GithubRepo> GetTopStarred()
         {
---
+++ Portfolio/wwwroot/js/Home.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    $('.item').first().addClass("active");
+    $('.indicator').first().addClass("active");
+
+    $(".info").hide();
+
+    $(".info-btn-about").click(function () {
+        $(".info-contact").hide();
+        $(".info-about").fadeToggle();
+    });
+
+    $(".info-btn-contact").click(function () {
+        $(".info-about").hide();
+        $(".info-contact").fadeToggle();
+    });
+});
